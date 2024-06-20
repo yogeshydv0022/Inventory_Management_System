999428870041429c8e0ccb4e53b316fdf655d43e
YogeshKumar <157680046+yogeshydv0022@users.noreply.github.com>
2024-06-21 04:15:00 +0530

    Update ImsProjectApplication.java

--- src/main/java/com/files/ImsProjectApplication.java
+++ src/main/java/com/files/ImsProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-//@EnableScheduling
+@EnableScheduling
 public class ImsProjectApplication {
 
 	public static void main(String[] args) {
